c75444771ff767cf3312e366265d3f0672dfa29e
dantabarez <dan.tabarez@gmail.com>
2022-10-18 11:41:32 -0500

    pequenos cambios

--- scripts/dia_mes.sh
+++ scripts/dia_mes.sh
@@ -6,7 +6,7 @@ for DIA in lunes martes miercoles jueves viernes sabado domingoo; do
   echo Dia de la semana: $DIA
 done
 
-for (( i = 0; i < 10; i++ )); do
+for ( i = 0; i < 10; i++ ); do
   #statements
   echo $i
 done
